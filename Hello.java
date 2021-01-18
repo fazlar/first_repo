7add46beda73af54c44b52dde00343e5635fe51e
unknown <fajlarabbi1991@gmail.com>
2021-01-18 22:29:57 +0600

    update java file

--- Hello.java
+++ Hello.java
@@ -1,9 +1,11 @@
-public class File {
+public class Hello {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter Your Nane: ");
         String name = reader.nextLine();
         System.out.println("Hello!" + name);
+        System.out.println("your name"+name)
+        System.out.println("your name"+name)
     }
 }
